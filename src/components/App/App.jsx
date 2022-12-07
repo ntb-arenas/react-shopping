83a741c808c90ab258d913aaddb2712568139c37
Niño Arenas <ntb.arenas@protonmail.com>
2022-12-07 23:31:02 +0000

    update navbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,19 +3,24 @@ import Card from "../Card/Card";
 import Navbar from "../Navbar/Navbar";
 
 function App() {
+  const [resourceType, setResourceType] = useState("");
   const [items, setItem] = useState([]);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/")
+    fetch(`https://fakestoreapi.com/products/${resourceType}`)
       .then((res) => res.json())
       .then((data) => setItem(data));
-  }, []);
+  }, [resourceType]);
 
-  console.log(items);
+  const handleClick = (category) => {
+    setResourceType(category);
+  };
 
   return (
-    <div className="container mx-auto font-poppins">
-      <Navbar />
+    <div className="container mx-auto mt-5 font-poppins">
+      <div className="text-center ">
+        <Navbar handleClick={handleClick} />
+      </div>
       <div className="grid grid-cols-1 gap-5 p-5 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
         {items.map((item) => {
           return <Card key={item.id} item={item} />;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Navbar = () => {
+const Navbar = ({ handleClick }) => {
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/categories")
+      .then((res) => res.json())
+      .then((data) => setCategory(data));
+  }, []);
+
+  // output
+  // [
+  //   "electronics",
+  //   "jewelery",
+  //   "men's clothing",
+  //   "women's clothing"
+  // ]
+
   return (
     <div>
-      Navbar
-      <button></button>
+      <button className="p-2 mx-2 border-2 rounded" onClick={(e) => handleClick("")}>
+        ALL PRODUCTS
+      </button>
+
+      {category.map((category, index) => {
+        return (
+          <button key={index} className="p-2 mx-2 border-2 rounded" onClick={(e) => handleClick(`category/${category}`)}>
+            {category.toUpperCase()}
+          </button>
+        );
+      })}
     </div>
   );
 };
+
 export default Navbar;
