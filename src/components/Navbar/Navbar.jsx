1ffc01d818980906c815834d3d132d12eb4bddb6
Niño Arenas <ntb.arenas@protonmail.com>
2022-12-10 15:28:49 +0000

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
-const Navbar = ({ handleClick, toggleValue }) => {
-  const [category, setCategory] = useState([]);
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products/categories")
-      .then((res) => res.json())
-      .then((data) => setCategory(data));
-  }, []);
+const Navbar = ({ products, filterProduct, toggleValue }) => {
+  const productCategory = products.map((product) => product.category);
+  const filteredCategory = [...new Set(productCategory)];
 
   return (
     <div className="text-xl divide-x divide-gray-300">
-      <button className="px-5 py-2" onClick={() => handleClick("")}>
+      <button className="px-5 py-2" onClick={() => filterProduct("all")}>
         ALL PRODUCTS
       </button>
 
-      {category.map((category, index) => (
-        <button key={index} className="px-5 py-2" onClick={() => handleClick(`/category/${category}`)}>
+      {filteredCategory.map((category, index) => (
+        <button key={index} className="px-5 py-2" onClick={() => filterProduct(`${category}`)}>
           {category.toUpperCase()}
         </button>
       ))}
