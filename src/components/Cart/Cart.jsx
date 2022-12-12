2ff713e5c8c9c1dfc9f759e80f3be9b651f1e299
Niño Arenas <ntb.arenas@protonmail.com>
2022-12-12 12:18:24 +0000

    Update cart images max width

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -17,9 +17,7 @@ const Cart = ({ cart, clearCart, isCartOpen, toggleCart }) => {
         {cart.map((product) => (
           <div key={product.id} className="flex justify-between mt-3">
             <div className="flex">
-              <div className="border max-w-[10rem] sm:max-w-[4.375px]  mr-2 p-2 bg-white">
-                <img src={product.image} alt="" />
-              </div>
+              <img src={product.image} alt="" className="border max-w-[3.1rem] sm:max-w-[4.375rem]  mr-2 p-2 bg-white" />
               <p className="text-xs sm:text-base">{product.title}</p>
             </div>
             <p className="ml-5 ">{product.qty}</p>
